f6df56fd4403354a84c1fbe5f93471e5136f6424
Caleb Spain <crspain@uh.edu>
2019-04-26 14:20:34 -0500

    Page Content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,17 +6,18 @@ import { DataService } from '../data.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
-  h1Style: boolean = false;
+  users: object;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
-  }
-
-  firstClick() {
-    this.data.firstClick();
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    });
   }
 
 }
