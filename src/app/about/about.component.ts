7b2b2bb86f5216c0417870f0ef31400b89178ed4
Caleb Spain <crspain@uh.edu>
2019-04-29 14:25:22 -0500

    about component updated

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  clicked = () => console.log("clicked");
+
   constructor() { }
 
   ngOnInit() {
